5ed7c7ff256766c2ebf909790524b46feb1109f3
Tor Morten Jensen <tormorten@anunatak.no>
2015-12-01 12:31:21 +0100

    switch to sage

--- functions.php
+++ functions.php
@@ -1,60 +1,28 @@
 <?php
 /**
- * anunatheme functions and definitions.
+ * Sage includes
  *
- * @link https://developer.wordpress.org/themes/basics/theme-functions/
+ * The $sage_includes array determines the code library included in your theme.
+ * Add or remove files to the array as needed. Supports child theme overrides.
  *
- * @package anunatheme
+ * Please note that missing files will produce a fatal error.
+ *
+ * @link https://github.com/roots/sage/pull/1042
  */
-$anunatheme_includes = array(
-	'inc/blade.php', // Blade functions
-	'inc/utils.php', // Utility functions
-	'inc/init.php', // Initial theme setup and constants
-	'inc/sidebar.php', // Sidebar class
-	'inc/config.php', // Configuration
-	'inc/activation.php', // Theme activation
-	'inc/titles.php', // Page titles
-	'inc/nav.php', // Custom nav modifications
-	'inc/pagination.php', // Custom pagination
-	'inc/gallery.php', // Custom [gallery] modifications
-	'inc/comments.php', // Custom comments modifications
-	'inc/scripts.php', // Scripts and stylesheets
-	'inc/extras.php', // Custom functions
-	'inc/acf.php', // Advanced Custom Fields
-	'inc/cpt.php', // Custom Post Type
-);
+$sage_includes = [
+  'lib/assets.php',    // Scripts and stylesheets
+  'lib/extras.php',    // Custom functions
+  'lib/setup.php',     // Theme setup
+  'lib/titles.php',    // Page titles
+  'lib/wrapper.php',   // Theme wrapper class
+  'lib/customizer.php' // Theme customizer
+];
 
-foreach ( $anunatheme_includes as $file ) {
-	if ( !$filepath = locate_template( $file ) ) {
-		trigger_error( sprintf( __( 'Error locating %s for inclusion', 'roots' ), $file ), E_USER_ERROR );
-	}
+foreach ($sage_includes as $file) {
+  if (!$filepath = locate_template($file)) {
+    trigger_error(sprintf(__('Error locating %s for inclusion', 'sage'), $file), E_USER_ERROR);
+  }
 
-	require_once $filepath;
+  require_once $filepath;
 }
-unset( $file, $filepath );
-
-
-/**
- * Implement the Custom Header feature.
- */
-require get_template_directory() . '/inc/custom-header.php';
-
-/**
- * Custom template tags for this theme.
- */
-require get_template_directory() . '/inc/template-tags.php';
-
-/**
- * Custom functions that act independently of the theme templates.
- */
-require get_template_directory() . '/inc/extras.php';
-
-/**
- * Customizer additions.
- */
-require get_template_directory() . '/inc/customizer.php';
-
-/**
- * Load Jetpack compatibility file.
- */
-require get_template_directory() . '/inc/jetpack.php';
+unset($file, $filepath);
--- search.php
+++ search.php
@@ -1,47 +1,14 @@
-<?php
-/**
- * The template for displaying search results pages.
- *
- * @link https://developer.wordpress.org/themes/basics/template-hierarchy/#search-result
- *
- * @package anunatheme
- */
+<?php get_template_part('templates/page', 'header'); ?>
 
-get_header(); ?>
+<?php if (!have_posts()) : ?>
+  <div class="alert alert-warning">
+    <?php _e('Sorry, no results were found.', 'sage'); ?>
+  </div>
+  <?php get_search_form(); ?>
+<?php endif; ?>
 
-	<section id="primary" class="content-area">
-		<main id="main" class="site-main" role="main">
+<?php while (have_posts()) : the_post(); ?>
+  <?php get_template_part('templates/content', 'search'); ?>
+<?php endwhile; ?>
 
-		<?php if ( have_posts() ) : ?>
-
-			<header class="page-header">
-				<h1 class="page-title"><?php printf( esc_html__( 'Search Results for: %s', 'anunatheme' ), '<span>' . get_search_query() . '</span>' ); ?></h1>
-			</header><!-- .page-header -->
-
-			<?php /* Start the Loop */ ?>
-			<?php while ( have_posts() ) : the_post(); ?>
-
-				<?php
-				/**
-				 * Run the loop for the search to output the results.
-				 * If you want to overload this in a child theme then include a file
-				 * called content-search.php and that will be used instead.
-				 */
-				get_template_part( 'template-parts/content', 'search' );
-				?>
-
-			<?php endwhile; ?>
-
-			<?php the_posts_navigation(); ?>
-
-		<?php else : ?>
-
-			<?php get_template_part( 'template-parts/content', 'none' ); ?>
-
-		<?php endif; ?>
-
-		</main><!-- #main -->
-	</section><!-- #primary -->
-
-<?php get_sidebar(); ?>
-<?php get_footer(); ?>
+<?php the_posts_navigation(); ?>
